642349851ea638278ae724328614df884463667b
Aaronkst <aaron.kst99@gmail.com>
2022-06-20 13:06:03 +0630

    api function

--- src/MyClass.php
+++ src/MyClass.php
@@ -16,3 +16,24 @@ class MyClass {
     return $this->variable;
   }
 }
+
+function api($payload, $base, $path, $headers) {
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_URL, $base . $path);
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array($payload)));
+
+  $response = curl_exec($ch);
+
+  $err = curl_error($ch);
+  curl_close($ch);
+  if ($err) {
+    throw $err;
+  } else {
+    return $response;
+  }
+}
