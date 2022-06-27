edd15d7f467faadc91575a713e95fea4eb37763f
Aaronkst <aaron.kst99@gmail.com>
2022-06-27 21:58:36 +0630

    typing and better declarations

--- src/MyClass.php
+++ src/MyClass.php
@@ -2,26 +2,37 @@
 
 namespace Aaron\FirstComposerPackage;
 
-class MyClass {
-  public $variable;
+class MyClass 
+{
+  public readonly string $key;
+  public readonly string $secret;
+  public readonly string $phone;
+  public readonly string $password;
+  private readonly string $prefix;
+  private readonly string $base;
+  private readonly boolean $production;
+  private readonly string $decryptKey;
+  private string $accesstoken;
+  private string $usertoken;
 
-  public function setVar($val): bool 
+  public function _construct(string $key, string $secret, string $phone, string $password, boolean $production = false, string $decryptKey)
   {
-    $this->variable = $val;
-    return true;
+    $this->key = $key;
+    $this->secret = $secret;
+    $this->phone = $phone;
+    $this->password = $password;
+    $this->prefix = $production ? implode("", ["/merchant","/1.0.0"]) : implode("", ["/om","/1.0.0"]);
+    $this->base = $production ? implode(".", ["api","ayapay","com"]) : implode(".", ["opensandbox","ayainnovation","com"]);
+    $this->production = $production;
+    $this->decryptKey = $decryptKey;
   }
 
-  public function getVar(): string 
+  public function getKey()
   {
-    return $this->variable;
-  }
-
-  public function getKey(): string
-  {
-    $data = array("grant_type"=>"client_credentials");
-    $base = "https://opensandbox.ayainnovation.com";
-    $path = "/token";
-    $headers = array(
+    $data: array = array("grant_type"=>"client_credentials");
+    $base: string = "https://opensandbox.ayainnovation.com";
+    $path: string = "/token";
+    $headers: array = array(
       "Authorization"=>"Basic WUc0WktjN1hXYjVDS0xPZUg4VGVRQjJLVVdRYTp0emtaT1J0X3hRb2FFOWNhVnhMbHRUOWt4SDhh",
       "Content-Type"=>"application/x-www-form-urlencoded"
     );
@@ -30,7 +41,8 @@ class MyClass {
   }
 }
 
-function api($payload, $base, $path, $headers) {
+function api(array $payload, string $base, string $path, array $headers): array 
+{
   $ch = curl_init();
 
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -40,6 +52,8 @@ function api($payload, $base, $path, $headers) {
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
   curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($payload));
   curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
+
+  print_r(http_build_query($payload));
 
   $response = curl_exec($ch);
 
